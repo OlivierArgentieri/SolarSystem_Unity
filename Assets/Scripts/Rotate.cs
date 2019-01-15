50f465b8d81ec14c070bb7d7791756e0c270543c
OlivierArgentieri <olivierargentieri@gmail.com>
2019-01-15 01:00:39 +0100

    [AOI] first commit

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    [SerializeField] protected float speed;
+
+    [SerializeField] protected bool reverseRotate = false;
+
+
+	// Update is called once per frame
+	void Update () {
+        if(reverseRotate)
+		    GetComponent<Transform>().Rotate(Vector3.up, -Time.deltaTime*speed);
+
+        else
+            GetComponent<Transform>().Rotate(Vector3.up, Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/RotationTrigo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTrigo : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject originGameObject;
+    
+    [SerializeField]
+    protected float vitesse;
+
+    [SerializeField]
+    protected float distance;
+
+    private float time;
+
+    
+	// Use this for initialization
+	void Start ()
+	{
+	    distance += originGameObject.transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    time = Time.time;
+	    float x = originGameObject.transform.position.x + Mathf.Cos(time * vitesse) * distance;
+	    float y = originGameObject.transform.position.y + 0;
+	    float z = originGameObject.transform.position.z + Mathf.Sin(time * vitesse) * distance;
+
+        GetComponent<Transform>().position = new Vector3(x, y, z);
+	}
+}

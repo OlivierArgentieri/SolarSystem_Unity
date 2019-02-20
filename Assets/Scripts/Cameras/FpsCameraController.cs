f7f3994c0bbbcc7b4171c5a3941122cdf2938074
OlivierArgentieri <30509073+OlivierArgentieri@users.noreply.github.com>
2019-02-20 10:18:14 +0100

    [OAI] Add zoom scroll on RTS

---
+++ Assets/Scripts/Cameras/FpsCameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsCameraController : MonoBehaviour {
+
+    [SerializeField]
+    private float m_speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        
+        float yr = Input.GetAxis("Mouse X");
+        float xr = Input.GetAxis("Mouse Y");
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        yr *= m_speed * Time.deltaTime;
+        xr *= m_speed * Time.deltaTime *-1;
+        x *= m_speed * Time.deltaTime;
+        z *= m_speed * Time.deltaTime;
+        transform.eulerAngles += new Vector3(xr, yr);
+        transform.Translate(new Vector3(x, 0, z));
+    }
+}
---
+++ Assets/Scripts/Cameras/RtsCameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RtsCameraController : MonoBehaviour
+{
+
+    [SerializeField] private float m_speed;
+    [SerializeField] private float m_max_zoom;
+    [SerializeField] private float m_min_zoom;
+    [SerializeField] private float m_sensivity_zoom;
+
+    private Camera m_camera_;
+
+    float x, z, w;
+    private void Awake()
+    {
+        m_camera_ = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        w += Input.GetAxis("Mouse ScrollWheel") * -1;
+
+        x *= m_speed * Time.deltaTime;
+        z *= m_speed * Time.deltaTime;
+
+        transform.Translate(new Vector3(x, z, 0));
+        
+    }
+    private void LateUpdate()
+    {
+        WheelZoom();
+    }
+
+    void WheelZoom()
+    {
+        if (m_min_zoom >0&& m_max_zoom >0 && m_sensivity_zoom >0)
+        {
+            w = Mathf.Clamp(w, m_min_zoom, m_max_zoom);
+            m_camera_.fieldOfView = w * m_sensivity_zoom;
+        }
+    }
+}
